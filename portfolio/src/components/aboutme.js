8647f584de83277e97792fe84188b30523e77872
Zach Partin <zachpartin@Zachs-MacBook-Pro.local>
2022-01-18 15:43:10 -0500

    progress

--- portfolio/src/components/aboutme.js
+++ portfolio/src/components/aboutme.js
@@ -11,14 +11,12 @@ const Aboutme = () => {
       <div className="aboutMeP">
         <p>Hi! I'm Zach (The one on the right) </p>
         <br />
-        <p>
-          I am a recent graduate of General Assembly's software engineering
-          immersive program, and am looking to break into the field and begin my career.
-        </p>
+        <p>I am a recent graduate of General Assembly's software engineering
+          immersive program, and am looking to break into the field and begin my career as a developer.</p>
         <br />
         <p>I graduated from Virginia Commonwealth University in 2020, and have a diverse background ranging from restaurant work to construction, as well as leadership positions on my college rugby team.</p>
         <br />
-        <p>I love taking on new challenges in all aspects of my life, and look forward to the opportunity to apply this attitude to a role as a software developer!</p>
+        <p>In my personal time, I enjoy weightlifting, hiking, and kickboxing. I love doing things that push me physically and mentally, and so far love the challenge that learning to program has provided me. I look forward to the opportunity to prove myself to be a competent and valuable member of a development team.</p>
       </div>
     </div>
   );
--- portfolio/src/components/portfolio.js
+++ portfolio/src/components/portfolio.js
@@ -15,43 +15,61 @@ const Portfolio = () => {
     <div className="portfolioLinks">
       
         <br />
+
+        
         <div className="portfolioPiece">
+
+        <a href="https://clever-clarke-51a296.netlify.app" className="portfolioLink">
         <span className="title">Virtual Christmas List</span>
-          <a href="https://clever-clarke-51a296.netlify.app" className="portfolioLink">
+          
           <img src={ChristmasList} className="portfolioImgs"/>
           <br/>
-          </a>
+          
           
           <br />
           
         <span className="portfolioDescription">An app for posting Christmas wishes</span>
+
+        </a>
         </div>
 
         <br />
         
         <div className="portfolioPiece">
+
+          <a href="https://jasz-library.netlify.app/" className="portfolioLink">
+            
           <span className="title">The Library</span>
-        <a href="https://jasz-library.netlify.app/" className="portfolioLink">
+      
           <img src={JASZLibrary} className="portfolioImgs"/>
           <br/>
-          </a>
+          
           
           <br />
           
           <span className="portfolioDescription">A full-CRUD app that allows users to edit an inventory of books</span>
-          </div>
+
+          </a>
+
+        </div>
         
         <br />
         
         <div className="portfolioPiece">
+
+          <a href="https://martial_mart.surge.sh/" className="portfolioLink">
+            
           <span className="title">Martial Mart</span>
-        <a href="https://martial_mart.surge.sh/" className="portfolioLink">
+        
           <img src={MartialMart}  className="portfolioImgs"/>
           <br/>
-        </a>
+        
         <br />
           <span className="portfolioDescription">Allows users to post used martial arts equipment for sale</span>
-          </div>
+
+          </a>
+
+        </div>
       </div>
   </div>
   )
